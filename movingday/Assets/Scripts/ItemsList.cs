fdc2d117a2004e20c2fcf6004790243f95dfa27a
Brian Cronin <programmingisgood@gmail.com>
2019-01-25 20:19:02 -0800

    WIP on truck logic

---
+++ movingday/Assets/Scripts/ItemsList.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "ItemsList", menuName = "movingday/ItemsList")]
+public class ItemsList : ScriptableObject
+{
+    [SerializeField]
+    private List<GameObject> itemPrefabs;
+}
---
+++ movingday/Assets/Scripts/Truck.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Truck : MonoBehaviour
+{
+    [SerializeField]
+    private List<ItemsList> unloadItemsOrder;
+}
