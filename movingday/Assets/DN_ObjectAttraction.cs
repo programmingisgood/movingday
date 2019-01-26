32dc567babec695b4112cedc9da090ec0fb17462
DESKTOP-2QGAPE9\Dang Nguyen <The_doomgard@yahoo.com>
2019-01-25 22:23:28 -0800

    Adding pulling object functionality

---
+++ movingday/Assets/DN_ObjectAttraction.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DN_ObjectAttraction : MonoBehaviour
+{
+    public float AttractorSpeed;
+    private bool MoveToward;
+    public GameObject Player;
+    public Transform PlayerRot;
+    private DN_PlayerMovement PlayerScripts;
+    public float turnSpeed;
+    public bool LeftPull;
+    public bool RightPull;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerScripts = Player.GetComponent<DN_PlayerMovement>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+         if (MoveToward && PlayerScripts.Pull && RightPull)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, AttractorSpeed * Time.deltaTime);
+            Vector3 dir = PlayerRot.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(dir);
+            Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+            transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        }
+        if (MoveToward && PlayerScripts.Pull && LeftPull)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, AttractorSpeed * Time.deltaTime);
+            Vector3 dir = PlayerRot.position - transform.position;
+            Quaternion lookRotation = Quaternion.LookRotation(-dir);
+            Vector3 rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+            transform.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            MoveToward = false;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            MoveToward = true;
+        }
+    }
+
+}
---
+++ movingday/Assets/DN_Triggers.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DN_Triggers : MonoBehaviour
+{
+    public bool RIghtTrigger;
+    public bool LeftTrigger;
+    public GameObject Furniture;
+    private DN_ObjectAttraction FurnitureScripts;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FurnitureScripts = Furniture.GetComponent<DN_ObjectAttraction>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        if(RIghtTrigger)
+    //        {
+    //            FurnitureScripts.RightPull = true;
+    //            FurnitureScripts.LeftPull = false;
+    //        }
+    //        if (LeftTrigger)
+    //        {
+    //            FurnitureScripts.RightPull = false;
+    //            FurnitureScripts.LeftPull = true;
+    //        }
+    //    }
+private void OnTriggerEnter(Collider other)
+{
+    if (other.tag == "Player")
+    {
+        if (RIghtTrigger)
+        {
+            FurnitureScripts.RightPull = true;
+            FurnitureScripts.LeftPull = false;
+        }
+        if (LeftTrigger)
+        {
+            FurnitureScripts.RightPull = false;
+            FurnitureScripts.LeftPull = true;
+        }
+    }
+}
+
+
+    
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Player")
+    //    {
+    //        if (RIghtTrigger)
+    //        {
+    //            FurnitureScripts.RightPull = false;
+    //            FurnitureScripts.LeftPull = false;
+    //        }
+    //        if (LeftTrigger)
+    //        {
+    //            FurnitureScripts.RightPull = false;
+    //            FurnitureScripts.LeftPull = false;
+    //        }
+    //    }
+    //}
+}
