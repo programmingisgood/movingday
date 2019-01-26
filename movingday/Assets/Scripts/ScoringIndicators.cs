eb21864300b4741ced564e02f7fc4263902b337e
Brian Cronin <programmingisgood@gmail.com>
2019-01-26 15:24:50 -0800

    Set up new art Started working on scoring indicator

---
+++ movingday/Assets/Scripts/ScoringIndicators.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScoringIndicators : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject scoringIndicatorExample;
+}
