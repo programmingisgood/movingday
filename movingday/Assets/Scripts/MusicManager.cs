c154e8de0710cc2ad6e7f8acd6c8cf8503cfc92e
Brian Cronin <programmingisgood@gmail.com>
2019-01-27 14:33:20 -0800

    Hooked up grunt sound effects Setup the music manager to play the second track in the game

---
+++ movingday/Assets/Scripts/MusicManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicManager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip track1 = null;
+
+    [SerializeField]
+    private AudioClip track2 = null;
+
+    [SerializeField]
+    private AudioSource source = null;
+
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Main Menu")
+        {
+            source.clip = track1;
+        }
+        else if (scene.name == "Main")
+        {
+            source.clip = track2;
+        }
+
+        source.Play();
+    }
+}
